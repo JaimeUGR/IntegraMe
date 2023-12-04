ade71756bc9ca80e49269294815355a315118e69
Jaime <92756555+JaimeUGR@users.noreply.github.com>
2023-12-04 18:57:20 +0100

    Fix Merge TaskRepositoryImpl.kt

--- app/src/main/java/com/integrame/app/tasks/data/repository/TaskRepositoryImpl.kt
+++ app/src/main/java/com/integrame/app/tasks/data/repository/TaskRepositoryImpl.kt
@@ -8,15 +8,6 @@ import com.integrame.app.tasks.data.model.TaskCard
 import com.integrame.app.tasks.domain.repository.TaskRepository
 import retrofit2.HttpException
 import com.integrame.app.core.data.network.api.IntegraMeApi
-<<<<<<< HEAD
-
-
-class TaskRepositoryImpl(private val api: IntegraMeApi) : TaskRepository {
-
-    suspend fun getTask(taskId: Int): Task {
-        TODO()
-    }
-=======
 import com.integrame.app.tasks.data.model.GenericTask
 import com.integrame.app.tasks.data.model.GenericTaskModel
 import com.integrame.app.tasks.data.model.MaterialTask
@@ -24,33 +15,21 @@ import com.integrame.app.tasks.data.model.MaterialTaskModel
 import com.integrame.app.tasks.data.model.MenuTaskModel
 import com.integrame.app.tasks.data.model.TaskModel
 
->>>>>>> adced9643fedba8407d43913dde986ee9eaf5a7f
-
 class TaskRepositoryImpl(private val api: IntegraMeApi): TaskRepository {
     // TODO: Incorporar api
     override suspend fun getPendingTaskCards(): AuthRequestResult<List<TaskCard>> {
         return AuthRequestResult.Authorized(FakeResources.taskCards)
     }
 
-<<<<<<< HEAD
-    suspend fun getMenuTask(taskId: Int): AuthRequestResult<MenuTask> {
-        return try {
-            AuthRequestResult.Authorized(api.getMenuTask(taskId))
-=======
     override suspend fun getTask(taskId: Int): AuthRequestResult<Task> {
         return try {
             AuthRequestResult.Authorized(api.getTask(taskId))
->>>>>>> adced9643fedba8407d43913dde986ee9eaf5a7f
         } catch (e: HttpException) {
             val statusCode = e.code()
             AuthRequestResult.Error("Error code: $statusCode")
         } catch (e: Exception) {
             AuthRequestResult.Error("Error: ${e.message ?: " desconocido"}")
         }
-<<<<<<< HEAD
-
-
-=======
     }
 
     override suspend fun getTaskModel(taskId: Int): AuthRequestResult<TaskModel> {
@@ -123,6 +102,5 @@ class TaskRepositoryImpl(private val api: IntegraMeApi): TaskRepository {
             is AuthRequestResult.Unauthorized -> AuthRequestResult.Unauthorized()
             is AuthRequestResult.Error -> AuthRequestResult.Error(requestResult.error)
         }
->>>>>>> adced9643fedba8407d43913dde986ee9eaf5a7f
     }
 }
